4548ad44e43c167d98e07adc7a297efb2b53b6ef
Girbaud Barthels <girbaud@rvised.com>
2023-05-31 20:32:24 +0200

    Added navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { animate, spring } from "motion"
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  count: number = 0;
 
+  verhoogTeller(): void {
+    this.count++;
+  }
+  hoverTitle(): void {
+    animate(".titleText", {
+      y: -50,
+      scale: 1.5
+    }, { easing: spring() })
+  }
+  leaveTitle(): void {
+    animate(".titleText", {
+      y: 0,
+      scale: 1
+    }, { easing: spring() })
+  }
+
+  funStuff(): void {
+    alert("Fun stufff")
+  }
 }
